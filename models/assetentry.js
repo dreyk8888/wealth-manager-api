d46a3de4c74d9322078c84a0938de3f2fe691c85
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-08-13 17:57:28 -0400

    Modified schema to add units, unitCost, currency

--- models/assetentry.js
+++ models/assetentry.js
@@ -3,9 +3,13 @@ var mongoose = require('mongoose');
 var AssetEntrySchema = new mongoose.Schema({
   assetType: String,
   assetName: String,
-  amount: Number,
+  units: Number,
+  unitCost: Number,
+  amount: Number,   //this will be calculated from units X unitCost
   category: String,
   date_purchased: { type: Date, default: Date.now },
+  currency: String,
+
 });
 
 module.exports = mongoose.model('AssetEntry', AssetEntrySchema);

148e6aeb8f1ba55db074ed2c42372cc785a1f27b
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-10-23 10:13:16 -0400

    Add defaults for asset and debt entries

--- models/assetentry.js
+++ models/assetentry.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
 
 var AssetEntrySchema = new mongoose.Schema({
-  class: String,
-  name: String,
-  units: Number,
-  unitCost: Number,
-  amount: Number,   //this will be calculated from units X unitCost
-  location: String,
+  class: {type: String, default: ""},
+  name: {type: String, default: ""},
+  units: {type: Number, default: 0},
+  unitCost: {type: Number, default: 0},
+  amount: {type: Number, default: 0},   //this will be calculated from units X unitCost
+  location: {type: String, default: "Domestic"},  //see /WealthManager/app/scripts/GlobalConstants.js for constant strings
   date_purchased: { type: Date, default: Date.now },
-  currency: String,
+  currency: {type: String, default: "USD"} //see WealthManager/app/scripts/GlobalConstants.js for constant strings
 
 });
 
--- models/debtentry.js
+++ models/debtentry.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 var DebtEntrySchema = new mongoose.Schema({
-  term: String,
-  name: String,
-  amount: Number   //this will be calculated from units X unitCost
+  term: {type: String, default: ""},
+  name: {type: String, default: ""},
+  amount: {type: Number, default: 0}   //this will be calculated from units X unitCost
 });
 
 module.exports = mongoose.model('DebtEntry', DebtEntrySchema);

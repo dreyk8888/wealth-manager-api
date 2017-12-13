ec929db5f4b603cf0863deae8201982d37493769
dreyk8888 <dreyk8888@users.noreply.github.com>
2017-12-13 01:13:44 -0500

    Add market price and value. Change amount to totalCost

--- models/assetentry.js
+++ models/assetentry.js
@@ -5,7 +5,9 @@ var AssetEntrySchema = new mongoose.Schema({
   name: {type: String, default: ""},
   units: {type: Number, default: 0},
   unitCost: {type: Number, default: 0},
-  amount: {type: Number, default: 0},   //this will be calculated from units X unitCost
+  marketPrice: {type: Number, default: 0},
+  totalCost: {type: Number, default: 0},   //this will be calculated from units X unitCost
+  marketValue: {type: Number, default: 0},
   location: {type: String, default: "Domestic"},  //see /WealthManager/app/scripts/GlobalConstants.js for constant strings
   date_purchased: { type: Date, default: Date.now },
   currency: {type: String, default: "USD"} //see WealthManager/app/scripts/GlobalConstants.js for constant strings
